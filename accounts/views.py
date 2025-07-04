4b29f0e861a75bb355122369f086c15b8b8e8c26
Anas <aanu8148@gmail.com>
2025-07-04 13:40:35 +0530

    updated the superuser creation logic

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from .serializers import SignupSerilizer,CustomLoginSerializer,UserProfileSerial
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
-
+from .models import Accounts
 class SignupView(APIView):
     permission_classes=[]
     
@@ -96,17 +96,16 @@ from django.contrib.auth import get_user_model
 class CreateSuperUserAPIView(APIView):
     permission_classes=[]
     def get(self, request):
-        User = get_user_model()
 
-        if not User.objects.filter(email="admin@example.com").exists():
-            User.objects.create_superuser(
-                email="admin@example.com",
-                username="adminuser",
-                password="admin1234",
-                first_name="Admin",
-                last_name="User",
-                phone_number="9999999999"
-            )
-            return Response({"message": "✅ Superuser created successfully."}, status=status.HTTP_201_CREATED)
-        return Response({"message": "⚠️ Superuser already exists."}, status=status.HTTP_200_OK)
+       
+        Accounts.objects.create_superuser(
+            email="admin@example.com",
+            username="adminuser",
+            password="admin1234",
+            first_name="Admin",
+            last_name="User",
+            phone_number="9999999999"
+        )
+        return Response({"message": "✅ Superuser created successfully."}, status=status.HTTP_201_CREATED)
+        
     
