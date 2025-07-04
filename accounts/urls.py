bcbee314f7bdc2f3191607d608b85bfa6ea67940
Anas <aanu8148@gmail.com>
2025-07-04 13:29:01 +0530

    new view created for superuser creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import SignupView,LoginView,LogoutView,UserProfileView,UserProfileEditView
+from .views import SignupView,LoginView,LogoutView,UserProfileView,UserProfileEditView,CreateSuperUserAPIView
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("logout/",LogoutView.as_view(),name="logout"),
     path('user/',UserProfileView.as_view(),name="user-profile"),
-    path('edit/user/',UserProfileEditView.as_view(),name="edit-profile")
+    path('edit/user/',UserProfileEditView.as_view(),name="edit-profile"),
+    
+    path('create-superuser/', CreateSuperUserAPIView.as_view()),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -83,3 +83,29 @@ class UserProfileEditView(APIView):
             }, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+    
+    
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import get_user_model
+
+class CreateSuperUserAPIView(APIView):
+    def get(self, request):
+        User = get_user_model()
+
+        if not User.objects.filter(email="admin@example.com").exists():
+            User.objects.create_superuser(
+                email="admin@example.com",
+                username="adminuser",
+                password="admin1234",
+                first_name="Admin",
+                last_name="User",
+                phone_number="9999999999"
+            )
+            return Response({"message": "✅ Superuser created successfully."}, status=status.HTTP_201_CREATED)
+        return Response({"message": "⚠️ Superuser already exists."}, status=status.HTTP_200_OK)
+    
